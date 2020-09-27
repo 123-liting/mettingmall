d8ff324fdd90709afe17755941839099324eea93
123-liting <707487736@qq.com>
2020-09-27 17:03:56 +0800

    liting

---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<!--
+ * @Author: sxb
+ * @Email: sxb@mail.com
+ * @Date: 2020-09-27 14:44:49
+ * @Descripttion: 
+-->
+<template>
+  <div id="app">
+    <img alt="Vue logo" src="./assets/logo.png">
+
+  </div>
+</template>
+
+<script>
+
+
+export default {
+  name: 'App',
+  components: {
+  
+  }
+}
+</script>
+
+<style>
+
+</style>
